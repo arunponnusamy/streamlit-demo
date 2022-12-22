a4dff253ebde682e6cf12ffc89f569d34757eeb6
Arun Ponnusamy <arunponnusamy@hotmail.com>
2022-12-22 15:42:31 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,27 @@
+import cv2
+import streamlit as st
+from PIL import Image
+import numpy as np
+
+st.header('Streamlit Demo')
+image = st.file_uploader('Upload an image', type=['png', 'jpg', 'jpeg'])
+
+face_detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+if image is not None:
+  img_pil = Image.open(image)
+  st.write('Original Image')
+  st.image(img_pil)
+
+  img_np = np.array(img_pil)
+  img_bgr = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
+  gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
+
+  faces = face_detector.detectMultiScale(gray, 1.6, 5)
+
+  for (x,y,w,h) in faces:  
+    cv2.rectangle(img_bgr, (x,y), (x+w,y+h), (0,255,0), 2)
+  img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+  st.image(img_rgb)
+  
+
